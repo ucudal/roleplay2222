5c152df02c4bc9ac417156cac4695407b9cf6bc7
inaki <inaki.garmendia@correo.ucu.edu.uy>
2025-09-22 09:55:23 -0300

    Agrego clase Elfo con ataque y defensa

---
+++ src/Library/Elfo.cs
@@ -0,0 +1,60 @@
+﻿namespace RoleplayGame.Library
+{
+    public class Arco
+    {
+        public int Danio = 20;
+    }
+
+    public class Escudo
+    {
+        public int Defensa = 10;
+    }
+
+    public class Elfo
+    {
+        public string Nombre;
+        public int HP;
+        public int MaxHP = 90;
+        public Arco Arco;
+        public Escudo Escudo;
+
+        public Elfo(string nombre)
+        {
+            Nombre = nombre;
+            HP = MaxHP;
+            Arco = new Arco();
+            Escudo = new Escudo();
+        }
+
+        public int DanioTotal()
+        {
+            return Arco.Danio;
+        }
+
+        public int DefensaTotal()
+        {
+            return Escudo.Defensa;
+        }
+
+        public string ResumenStats()
+        {
+            return "Elfo " + Nombre + " → Vida: " + HP + " | Daño: " + DanioTotal() + " | Defensa: " + DefensaTotal();
+        }
+
+        public void Cura()
+        {
+            HP += 20;
+            if (HP > MaxHP) HP = MaxHP;
+        }
+
+        public void Atacar(Elfo objetivo)
+        {
+            int danioReal = DanioTotal() - objetivo.DefensaTotal();
+            if (danioReal < 0) danioReal = 0; // no puede curar al atacar
+            objetivo.HP -= danioReal;
+            if (objetivo.HP < 0) objetivo.HP = 0;
+
+            Console.WriteLine(Nombre + " ataco a " + objetivo.Nombre + " dañando " + danioReal + " de daño");
+        }
+    }
+}
