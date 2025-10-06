eff8b01c677df8eb0ea08f3476703786f2ce1fd7
Sebastian <SEBASTIAN.SILVEIRA@correo.ucu.edu.uy>
2025-10-05 23:27:37 -0300

    Actualizado proyecto con csproj arreglado

---
+++ src/Library/Ipersonaje.cs
@@ -0,0 +1,21 @@
+namespace RoleplayGame.Library;
+
+public interface ICharacter
+{
+    string Nombre { get; set; }
+    int MaxHP { get; set; }
+    int CurrentHP { get; set; }
+    List<IItem> Equipamiento { get; set; }
+
+    void AddItem(IItem item);
+    void RemoveItem(IItem item);
+    void HealSelf();
+    void ReceiveAttack(ICharacter atacante);
+    void Attack(ICharacter objetivo);
+}
+
+public interface IMagicCharacter : ICharacter
+{
+    void AddMagicItem(IMagicItem mItem);
+    void RemoveMagicItem(IMagicItem mItem);
+}
